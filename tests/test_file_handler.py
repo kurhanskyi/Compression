be27f9a4635b5c08749bf22b82b6aa9cadf4bc3d
kurhanskyi <kurganskijmatvej5@gmail.com>
2024-10-24 22:22:30 +0300

    Update test_file_handler.py

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -2,25 +2,20 @@ import unittest
 import os
 from utils.file_handler import FileHandler
 
-class TestFileHandler(unittest.TestCase):
-    def setUp(self):
-        self.test_file = 'test_file.txt'
-        self.test_data = b'Hello, World!'
 
-    def test_write_and_read_file(self):
-        FileHandler.write_file(self.test_file, self.test_data)
-        read_data = FileHandler.read_file(self.test_file)
-        self.assertEqual(read_data, self.test_data)
+def test_write_and_read_file(self):
+    FileHandler.write_file(self.test_file, self.test_data)
+    read_data = FileHandler.read_file(self.test_file)
+    self.assertEqual(read_data, self.test_data)
 
-    def test_get_file_size(self):
-        FileHandler.write_file(self.test_file, self.test_data)
-        size = FileHandler.get_file_size(self.test_file)
-        self.assertEqual(size, len(self.test_data))
-
-    def test_delete_file(self):
-        FileHandler.write_file(self.test_file, self.test_data)
-        FileHandler.delete_file(self.test_file)
-        self.assertFalse(os.path.exists(self.test_file))
+def test_get_file_size(self):
+    FileHandler.write_file(self.test_file, self.test_data)
+    size = FileHandler.get_file_size(self.test_file)
+    self.assertEqual(size, len(self.test_data))
+def test_delete_file(self):
+    FileHandler.write_file(self.test_file, self.test_data)
+    FileHandler.delete_file(self.test_file)
+    self.assertFalse(os.path.exists(self.test_file))
 
     def tearDown(self):
         if os.path.exists(self.test_file):
